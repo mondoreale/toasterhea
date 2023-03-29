fcdcb290caf7348aca35d50740d0d76344b7eda9
Mariusz Roliński <mariusz@mondoreale.pl>
2023-03-30 01:56:29 +0200

    Show what I got

---
+++ rollup.config.mjs
@@ -0,0 +1,43 @@
+import dts from 'rollup-plugin-dts'
+import esbuild from 'rollup-plugin-esbuild'
+import babel from '@rollup/plugin-babel'
+
+function bundle(config) {
+    return {
+        ...config,
+        external: [/react/],
+    }
+}
+
+export default [
+    bundle({
+        input: './src/index.tsx',
+        plugins: [
+            esbuild(),
+            babel({
+                babelHelpers: 'bundled',
+                extensions: ['.ts', '.tsx'],
+            }),
+        ],
+        output: [
+            {
+                file: `./index.js`,
+                format: 'es',
+            },
+            {
+                file: './index.cjs',
+                format: 'cjs'
+            }
+        ],
+    }),
+    bundle({
+        input: './src/index.tsx',
+        plugins: [dts()],
+        output: [
+            {
+                file: `./index.d.ts`,
+                format: 'es',
+            },
+        ],
+    }),
+]
